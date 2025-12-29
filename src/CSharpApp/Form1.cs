0bb0d7ed43d8d2462aca603e457223cd4e452992
钱多多 <qiandafu@hotmail.com>
2025-12-29 17:06:25 +0800

    *demo中修改英文变量名称与文档中心一直

--- src/CSharpApp/Form1.cs
+++ src/CSharpApp/Form1.cs
@@ -25,18 +25,18 @@ namespace CSharpApp
             AdvertManager.AdClickEvent += AdvertManager_AdClickEvent;
             AdvertManager.AdCloseEvent += AdvertManager_AdCloseEvent;
         }
-        //Production environment
-        private const string YourAppId = "69316b6861328938223cc124";
-        private const string YourSecretKey = "MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgZgULOuiIDYZyGiUyYdGr3odHVN6ebZ1uDwXx7PXiHh2gCgYIKoZIzj0DAQehRANCAASf1FWCfsSn/tXFVRt04C7JkpRG12KSC3wnaJRWb5QWin9dsBk1OR31BCsELMYtWsFhA7e6Q6Fi4Mi6+ub24O5a";
-        private const string SplashScreenUnitId = "b871f83c5e8845f1b43325561bcdd6c7";     //Splash Ad:1920 x 1080
-        private const string ExitScreenUnitId = "5076eab6ae1042b6b92f73ea01981475";       //Exit Ad:1920 x 1080
-        private const string BannerUnitId = "cb7d9688a2d9499992febb6b642b3625";           //Banner Ad:728 x 90
-        private const string InterstitialUnitId = "2cb66a1301404561881a3f26b6ce5ba7";     //Interstitial Ad:1024 x 768
-        private const string CoupletUnitId = "b502f6e6281c43e4b28ea22503471039";          //Couple Ad:300 x 600
-        private const string RewardUnitId = "2ae60936ba664fbfb7d92ce3a19c2915";           //Rewarded Ad:1024x768
-        private const string InformationFlowUnitId = "f152f6caf7a8440f8510bc31534baf4e";  //Feed，Developers need to maintain the advertising control.
-        private const string EmbeddedUnitId = "4192966a9db343f48dd2f6308ea9ec30";         //Embedded，Developers need to maintain the advertising control.
-        
+        //Parameters are created in the developer backend. https://developer.ad.mguwp.net
+        private const string YourAppId = "692e5d6a207c9dd383ba56f7";
+        private const string YourSecretKey = "MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQg9pm4A6JkgQr7Xx5UUmX/NT+ZKM+ZF/2btAIBsdrJF76gCgYIKoZIzj0DAQehRANCAARrjJmtngZzxhRAa0Wn99ZN7QGf9ozmvghuvaicqFmA3j35XDkfXBgIqMTABogfpd+1LrAADeXkgOPzqw6b12my";
+        private const string SplashAdUnitId = "9ad41e3410084523a4f2a13ca65df395";         //Splash Ad:1920 x 1080
+        private const string ExitAdUnitId = "a0e4a92613674feab0e3eaa36d1c17b8";           //Exit Ad:1920 x 1080
+        private const string BannerUnitId = "948ba7ccdfa34a0b8e2f96a66244c494";           //Banner Ad:728 x 90
+        private const string InterstitialUnitId = "10494292d2d9431691c3bebf0f35815c";     //Interstitial Ad:1024 x 768
+        private const string CoupletUnitId = "ef5f566b0eb14132987efbd57d6af60f";          //Couple Ad:300 x 600
+        private const string RewardedUnitId = "a9bd7d57faef4f8cb016979284c86102";         //Rewarded Ad:1024x768
+        private const string FeedUnitId = "e13d9a6a4dbd42c2bd50561773dbda40";             //Feed，Developers need to maintain the advertising control.
+        private const string EmbeddedUnitId = "eb60d1e936c044adb696d9fa8147d590";         //Embedded，Developers need to maintain the advertising control.
+
         private void AdvertManager_AdCloseEvent(object sender, string e)
         {
             ShowMessage("The ad has been disabled. " + e);
@@ -47,7 +47,7 @@ namespace CSharpApp
             JObject jsonObject = JObject.Parse(e);
             string unitId = (string)jsonObject["unitId"];
 
-            if (unitId == RewardUnitId)//Rewarded Ad, with rewards distributed based on the results returned.
+            if (unitId == RewardedUnitId)//Rewarded Ad, with rewards distributed based on the results returned.
             {
                 int completeStatus = (int)jsonObject["completeStatus"];
                 string resourceId = (string)jsonObject["resourceId"];
@@ -88,17 +88,28 @@ namespace CSharpApp
                 ShowMessage($"Initialization complete:Token={ApplicationManager.AccessToken.Token}, ExpiresIn={ApplicationManager.AccessToken.ExpiresIn}");
 
                 //Step 1: After successful initialization, load the Exit ad resources.
-                AdvertManager.SetupExitAd(ExitScreenUnitId);
+                AdvertManager.SetupExitAd(ExitAdUnitId);
 
                 //...
 
 
-                AdvertManager.ShowAd(this, SplashScreenUnitId, AdType.FullScreen);//Splash Ad
+                AdvertManager.ShowAd(this, SplashAdUnitId, AdType.FullScreen);//Splash Ad
             }
             else
             {
                 ShowMessage("Initialization failed");
             }
+        }
+
+        /// <summary>
+        /// Exit ad
+        /// Step 2. Display a Exit ad when the app closes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _ = AdvertManager.ShowExitAdBlocking();
         }
 
         /// <summary>
@@ -108,16 +119,16 @@ namespace CSharpApp
         /// <param name="e"></param>
         private void btnAd1_Click(object sender, EventArgs e)
         {
-            AdvertManager.ShowAd(this, SplashScreenUnitId, AdType.FullScreen);
+            AdvertManager.ShowAd(this, SplashAdUnitId, AdType.FullScreen);
         }
         private void btnAd1_2_Click(object sender, EventArgs e)
         { 
-            AdvertManager.ShowAd(this, "{\"unitId\": \"" + SplashScreenUnitId + "\",\"media\":\"video\"}", AdType.FullScreen);
+            AdvertManager.ShowAd(this, "{\"unitId\": \"" + SplashAdUnitId + "\",\"media\":\"video\"}", AdType.FullScreen);
         }
 
         private void btnAd1_3_Click(object sender, EventArgs e)
         {
-            AdvertManager.ShowAd(this, "{\"unitId\": \"" + SplashScreenUnitId + "\",\"media\":\"web\"}", AdType.FullScreen);
+            AdvertManager.ShowAd(this, "{\"unitId\": \"" + SplashAdUnitId + "\",\"media\":\"web\"}", AdType.FullScreen);
         }
 
         /// <summary>
@@ -181,13 +192,13 @@ namespace CSharpApp
         {
             string comment = "id123,abc,$9.99";//Transparent parameters require URL encoding.
             comment = Uri.EscapeDataString(comment);
-            AdvertManager.ShowAd(this, "{\"unitId\": \"" + RewardUnitId + "\",\"comment\":\"" + comment + "\"}", AdType.Reward);
+            AdvertManager.ShowAd(this, "{\"unitId\": \"" + RewardedUnitId + "\",\"comment\":\"" + comment + "\"}", AdType.Reward);
         }
         private void btnAd6_2_Click(object sender, EventArgs e)
         {
             string comment = "id123,abc,$9.99";
             comment = Uri.EscapeDataString(comment);
-            AdvertManager.ShowAd(this, "{\"unitId\": \"" + RewardUnitId + "\",\"comment\":\"" + comment + "\",\"media\":\"web\"}", AdType.Reward);
+            AdvertManager.ShowAd(this, "{\"unitId\": \"" + RewardedUnitId + "\",\"comment\":\"" + comment + "\",\"media\":\"web\"}", AdType.Reward);
         }
 
         //Feed
@@ -195,7 +206,7 @@ namespace CSharpApp
         {
             dynamic jsonObj = new
             {
-                unitId = InformationFlowUnitId,//Customizable dimensions, example: MG backend settings 400×50
+                unitId = FeedUnitId,//Customizable dimensions, example: MG backend settings 400×50
                 media = "image",
                 width = panelAd6.Width,
                 height = panelAd6.Height
@@ -207,7 +218,7 @@ namespace CSharpApp
         {
             dynamic jsonObj = new
             {
-                unitId = InformationFlowUnitId,
+                unitId = FeedUnitId,
                 media = "web",
                 width = panelAd6_2.Width,
                 height = panelAd6_2.Height
@@ -283,18 +294,7 @@ namespace CSharpApp
                     this.Controls.Remove(item);
                 }
             }
-        } 
-
-        /// <summary>
-        /// Exit ad
-        /// Step 2. Display a Exit ad when the app closes.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            _ = AdvertManager.ShowExitAdBlocking();
-        }
+        }  
 
         private void ShowMessage(string message)
         {
